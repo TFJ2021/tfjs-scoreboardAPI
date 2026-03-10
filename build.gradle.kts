e58931dee032354de6e266e88ed2085efe86a0b8
TFJ_5183 <127980138+TFJ2021@users.noreply.github.com>
2026-03-10 17:58:47 +0100

    feat: jitpack intigration

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,13 +7,6 @@ plugins {
 group = "net.tfj"
 version = "1.0"
 
-repositories {
-    mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/") {
-        name = "papermc-repo"
-    }
-}
-
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
